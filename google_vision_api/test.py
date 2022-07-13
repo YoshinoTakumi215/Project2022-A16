84876e9af668e187885417d2a4234da866f1866d
YoshinoTakumi215 <72242152+YoshinoTakumi215@users.noreply.github.com>
2022-07-13 10:21:02 +0900

    Add files via upload

--- google_vision_api/test.py
+++ google_vision_api/test.py
@@ -1,21 +1,23 @@
 from google.cloud import vision
 from google.oauth2 import service_account
- 
-IMG_URL = "https://imageslabo.com/wp-content/uploads/2019/05/553_dog_chihuahua_7203-973x721.jpg"
- 
+import io
+IMG_URL =  'download.jpg'
+from google.cloud.vision_v1 import types
 # 身元証明書のjson読み込み
-credentials = service_account.Credentials.from_service_account_file('/Users/c0p31102/Desktop/Key.json')
+credentials = service_account.Credentials.from_service_account_file('Key.json')
  
 client = vision.ImageAnnotatorClient(credentials=credentials)
 image = vision.Image()
-image.source.image_uri = IMG_URL
- 
+with io.open(IMG_URL, 'rb') as image_file:
+    content = image_file.read()
+image = types.Image(content=content)
 response = client.label_detection(image=image)
 labels = response.label_annotations
  
- 
+print("-------------画像解析結果--------------")
 for label in labels:
-    print(label.description + ":" + str(label.score))
+    print(label.description + " : " + str(label.score))
+    print("---------------------------------------")
  
 if response.error.message:
     raise Exception(
