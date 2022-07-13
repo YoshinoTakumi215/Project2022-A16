09a5004829ff7dab1ac277747f0ed98eacf0b0f5
YoshinoTakumi215 <72242152+YoshinoTakumi215@users.noreply.github.com>
2022-07-13 09:45:22 +0900

    Add files via upload
    Google Vision APIのテスト

---
+++ google_vision_api/test.py
@@ -0,0 +1,24 @@
+from google.cloud import vision
+from google.oauth2 import service_account
+ 
+IMG_URL = "https://imageslabo.com/wp-content/uploads/2019/05/553_dog_chihuahua_7203-973x721.jpg"
+ 
+# 身元証明書のjson読み込み
+credentials = service_account.Credentials.from_service_account_file('/Users/c0p31102/Desktop/Key.json')
+ 
+client = vision.ImageAnnotatorClient(credentials=credentials)
+image = vision.Image()
+image.source.image_uri = IMG_URL
+ 
+response = client.label_detection(image=image)
+labels = response.label_annotations
+ 
+ 
+for label in labels:
+    print(label.description + ":" + str(label.score))
+ 
+if response.error.message:
+    raise Exception(
+        '{}\nFor more info on error messages, check: '
+        'https://cloud.google.com/apis/design/errors'.format(
+            response.error.message))
